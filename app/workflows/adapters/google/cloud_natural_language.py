3a086568632c736f78fe30a8f953a706b234ac3b
doko123 <dominika15kowalczyk@gmail.com>
2018-07-09 22:28:41 +0400

    2-feature/setup_ci_tests: Add circle ci tests and missing files

---
+++ app/workflows/adapters/google/cloud_natural_language.py
@@ -0,0 +1,13 @@
+"""
+This solution supports only 10 languages:
+Chinese (Simplified)	zh
+Chinese (Traditional)	zh-Hant
+English	en
+French	fr
+German	de
+Italian	it
+Japanese	ja
+Korean	ko
+Portuguese	pt
+Spanish	es
+"""
---
+++ app/workflows/views/__init__.py
@@ -0,0 +1,2 @@
+__all__ = "HealthCheckResource"
+from workflows.views.core import HealthCheckResource  # noqa
