caec6c603a415f993f0aefd803c29bb4107bab06
Dominika Kowalczyk <dominika15kowalczyk@gmail.com>
2018-07-14 21:47:17 +0400

    3-feature/initial_comits: Extend makefile with update requirements, call black, add gunicorn, elasticsearch, add tests for es, dockerize es, fix tests (#5)

---
+++ app/tests/conftest.py
@@ -0,0 +1,18 @@
+from config import create_app
+import pytest
+from funcy import first, second
+
+
+@pytest.fixture(scope="session")
+def setup_env():
+    return create_app.create_app()
+
+
+@pytest.fixture(scope="session")
+def app(setup_env):
+    return first(setup_env)
+
+
+@pytest.fixture(scope="session")
+def es(setup_env):
+    return second(setup_env)
