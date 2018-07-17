be0ef50ef8afc47d11208bd6245f456641db109d
doko123 <dominika15kowalczyk@gmail.com>
2018-07-17 19:29:22 +0400

    5-feature/create_view_for_core_functionality: Create hotel.html and view for it, add debug option to creating app, place initial usecase

---
+++ app/workflows/use_cases/review_hotel.py
@@ -0,0 +1,10 @@
+
+
+class HotelReviewUseCase:
+
+    def review_hotel(self):
+        # fetch_comments: list of contents
+        # MicrosoftManager().detect_languages() for comments
+        # sentiment = MicrosoftManager().get_sentiment_measured()
+        # return sentiment
+        return
--- app/workflows/views/core.py
+++ app/workflows/views/core.py
@@ -1,7 +1,25 @@
-from flask import jsonify
+from flask import jsonify, Flask, Response, render_template, redirect, url_for, request
 from flask_restful import Resource
 
 
+
 class HealthCheckResource(Resource):
+    methods = ["GET"]
+
     def get(self):
+        # TODO: Change to return number of cached hotels
         return jsonify({"status": "OK"})
+
+
+class HomePageResource(Resource):
+    methods = ["GET", "POST"]
+
+    def get(self):
+
+        return Response(render_template("home.html"), mimetype="text/html")
+
+    def post(self):
+        if not request.form["hotel_name"]:
+            return redirect(url_for("homepageresource"))
+        
+
